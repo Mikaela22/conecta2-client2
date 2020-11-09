c06d60735ff2a24979b43b98595ea9de6af8844c
Mikaela22 <61974476+Mikaela22@users.noreply.github.com>
2020-11-09 00:35:53 -0300

    initialize

--- conecta2-client2/src/index.js
+++ conecta2-client2/src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { combineReducers, createStore } from "redux";
+import { Provider } from "react-redux";
+import userReducer from "./redux/reducers/userReducer";
+import tweetReducer from "./redux/reducers/tweetReducer";
+
+/* const rootReducer = combineReducers({
+  userReducer,
+  tweetReducer,
+}); */
+const store = createStore(userReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.unregister();
